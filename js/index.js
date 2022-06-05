a9e189aea95e06fc1dfbda7f3d987aad1e9f2e60
unknown <r0781309@student.thomasmore.be>
2022-06-05 13:40:24 +0200

    Taalfouten gecorrigeerd.

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ var link = document.createElement('a');
 link.href = url;
 link.download = 'cv.pdf';
 link.dispatchEvent(new MouseEvent('click'));
+
+//Typing title
+var typed = new Typed(".auto-typing", {
+    strings: ["Murrel Venlo"],
+    v: 100,
+    backSpeed: 100,
+    loop: true
+})
